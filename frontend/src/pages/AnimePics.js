08f5e7adfd124e2027d5f4a15b07bbf706eae56a
Evandro Lugli <evandro.lugli@gmail.com>
2024-11-13 16:47:19 +1100

    update

--- frontend/src/pages/AnimePics.js
+++ frontend/src/pages/AnimePics.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Carousel } from 'react-bootstrap';
+import img1 from '../images/img-01.jpg';
+import img2 from '../images/img-02.jpg';
+import img3 from '../images/img-03.jpg';
 
-const AnimePics = () => {
-  const animeImages = [
-    'path/to/anime1.jpg',
-    'path/to/anime2.jpg',
-    'path/to/anime3.jpg',
-  ];
-
-  return (
-    <Container className="mt-5">
-      <h2>Anime Pics</h2>
-      <Row>
-        {animeImages.map((img, index) => (
-          <Col key={index} xs={12} sm={6} md={4}>
-            <Image src={img} alt={`Anime ${index + 1}`} fluid className="mb-3" />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+const AnimePics = () => (
+  <Container className="mt-5">
+    <h2 className="text-center mb-4">Anime Pics</h2>
+    <Carousel>
+      {[img1, img2, img3].map((img, idx) => (
+        <Carousel.Item key={idx}>
+          <img
+            className="d-block w-100"
+            src={img}
+            alt={`Anime ${idx + 1}`}
+            style={{ height: '400px', objectFit: 'cover' }} // Set fixed height
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  </Container>
+);
 
 export default AnimePics;
